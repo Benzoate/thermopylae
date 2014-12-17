2f1983a6b8c83234de0863a223c5a6e8e6235443
Michael Purdy <michael@mrmichael.co.uk>
2014-12-17 22:41:34 +0000

    durr

--- thermopylae/urls.py
+++ thermopylae/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns(
     '',
-    url(r'^temperature/$', include('temperature.urls')),
+    url(r'^temperature/', include('temperature.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
