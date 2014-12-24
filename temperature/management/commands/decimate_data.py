7d7d2fdaa3158fe67b10eb2f1c167b28aa569809
Michael Purdy <michael@mrmichael.co.uk>
2014-12-24 23:06:29 +0000

    print exception

--- temperature/management/commands/decimate_data.py
+++ temperature/management/commands/decimate_data.py
@@ -11,34 +11,37 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        therms = models.Thermocouple.objects.all()
-        for therm in therms:
-            print 'on thermocouple: %s' % therm
-            current = datetime.date.today() - datetime.timedelta(days=7)
-            readings = list(models.Reading.objects.filter(date__lt=current,
-                                                          thermocouple=therm)
-                            .order_by('-date').all())
-            while readings:
-                todays = [i for i in readings if i.date.date() == current]
-                if not todays:
+        try:
+            therms = models.Thermocouple.objects.all()
+            for therm in therms:
+                print 'on thermocouple: %s' % therm
+                current = datetime.date.today() - datetime.timedelta(days=7)
+                readings = list(models.Reading.objects.filter(date__lt=current,
+                                                              thermocouple=therm)
+                                .order_by('-date').all())
+                while readings:
+                    todays = [i for i in readings if i.date.date() == current]
+                    if not todays:
+                        current = current - datetime.timedelta(days=1)
+                        continue
+                    values = map(lambda x: x.value, todays)
+                    print 'collating %s values' % (len(values),)
+
+                    col = models.CollatedReading()
+                    col.max = max(values)
+                    col.min = min(values)
+                    col.mean = sum(values) / len(values)
+                    col.date = current
+                    col.thermocouple = therm
+                    col.save()
+
+                    print 'min: %s, max: %s, mean: %s' % (col.min, col.max,
+                                                          col.mean)
+
+                    readings = [i for i in readings if i not in todays]
+                    for r in todays:
+                        r.delete()
+
                     current = current - datetime.timedelta(days=1)
-                    continue
-                values = map(lambda x: x.value, todays)
-                print 'collating %s values' % (len(values),)
-
-                col = models.CollatedReading()
-                col.max = max(values)
-                col.min = min(values)
-                col.mean = sum(values) / len(values)
-                col.date = current
-                col.thermocouple = therm
-                col.save()
-
-                print 'min: %s, max: %s, mean: %s' % (col.min, col.max,
-                                                      col.mean)
-
-                readings = [i for i in readings if i not in todays]
-                for r in todays:
-                    r.delete()
-
-                current = current - datetime.timedelta(days=1)
+        except BaseException as e:
+            print e
